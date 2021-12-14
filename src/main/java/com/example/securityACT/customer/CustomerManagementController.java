9e1fdbe611699628dab945ebd60a058e4f232664
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-14 14:40:55 +0100

    added customer management controller

---
+++ src/main/java/com/example/securityACT/customer/CustomerManagementController.java
@@ -0,0 +1,38 @@
+package com.example.securityACT.customer;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/customers")
+public class CustomerManagementController {
+
+    private static final List<Customer> CUSTOMERS = Arrays.asList(
+            new Customer(1, "Fenn Lazarus"),
+            new Customer(2, "Eline Hermans"),
+            new Customer(3, "Charles Xavier")
+    );
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return CUSTOMERS;
+    }
+
+    @PostMapping
+    public void registerNewCustomer(@RequestBody Customer customer) {
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer customerId) {
+        System.out.println(customerId);
+    }
+
+    @PutMapping(path = "{customerId}")
+    public void updateCustomer(@PathVariable("customerId")Integer customerId, @RequestBody Customer customer) {
+        System.out.println(String.format("%s %s", customer, customer));
+    }
+
+}
