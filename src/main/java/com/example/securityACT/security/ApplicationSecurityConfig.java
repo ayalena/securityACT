4ac3abef8a692ce93a4cb78e22cd82c167169de3
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-14 12:35:18 +0100

    added custom user

--- src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.securityACT.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +26,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
                 ;
+    }
 
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails fennLazarusUser = User.builder()
+                .username("fennlazarus")
+                .password("password")
+                .roles("CUSTOMER") // ROLE_CUSTOMER
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                fennLazarusUser
+        );
     }
 }
