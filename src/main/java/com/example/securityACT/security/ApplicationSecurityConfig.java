47719782821938d521795849bf9b5b2f5ace1808
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-14 14:19:12 +0100

    added role based authentication

--- src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
@@ -30,9 +30,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
-
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers(("/api/**")).hasRole(CUSTOMER.name())
                 .anyRequest()
                 .authenticated()
                 .and()
