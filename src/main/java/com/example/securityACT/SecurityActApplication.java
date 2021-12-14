1eef17d767e689f9f8a1fff949fac70d6bcbafcd
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-14 11:02:04 +0100

    base

---
+++ src/main/java/com/example/securityACT/SecurityActApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityACT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityActApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityActApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityACT/SecurityActApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityACT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityActApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
