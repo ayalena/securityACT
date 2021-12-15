7fe01215c4ffab51ae517dc779582b081c41de5e
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-15 15:15:08 +0100

    preauthorisation instead of antmatchers try-out

--- src/main/java/com/example/securityACT/customer/CustomerManagementController.java
+++ src/main/java/com/example/securityACT/customer/CustomerManagementController.java
@@ -1,5 +1,6 @@
 package com.example.securityACT.customer;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -15,25 +16,34 @@ public class CustomerManagementController {
             new Customer(3, "Charles Xavier")
     );
 
+//    hasRole("ROLE_")
+//    hasAnyRole("ROLE_")
+//    hasAuthority("permission")
+//    hasAnyAuthority("permission")
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Customer> getAllCustomers() {
         System.out.println("getAllCustomers");
         return CUSTOMERS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('customer:write')")
     public void registerNewCustomer(@RequestBody Customer customer) {
         System.out.println("registerNewCustomer");
         System.out.println(customer);
     }
 
     @DeleteMapping(path = "{customerId}")
+    @PreAuthorize("hasAuthority('customer:write')")
     public void deleteCustomer(@PathVariable("customerId") Integer customerId) {
         System.out.println("deleteCustomer");
         System.out.println(customerId);
     }
 
     @PutMapping(path = "{customerId}")
+    @PreAuthorize("hasAuthority('customer:write')")
     public void updateCustomer(@PathVariable("customerId")Integer customerId, @RequestBody Customer customer) {
         System.out.println("updateCustomer");
         System.out.println(String.format("%s %s", customerId, customer));
--- src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import static com.example.securityACT.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -38,10 +40,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(("/api/**")).hasRole(CUSTOMER.name())
 
                 //AUTHORITIES
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(PRODUCT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
 
                 .anyRequest()
                 .authenticated()
