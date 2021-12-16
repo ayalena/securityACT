fffbb519598b869887a5003fa698150ccafcf668
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-16 12:19:02 +0100

    added dao auth provider

--- src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityACT/security/ApplicationSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.securityACT.security;
 
+import com.example.securityACT.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,10 +28,13 @@ import static com.example.securityACT.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -81,33 +87,52 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails fennLazarusUser = User.builder()
-                .username("fennlazarus")
-                .password(passwordEncoder.encode("password"))
-//                .roles(CUSTOMER.name()) // ROLE_CUSTOMER
-                .authorities(CUSTOMER.getGrantedAuthorities())
-                .build();
-
-        UserDetails kimUser = User.builder()
-                .username("kim")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name()) // ROLE_ADMIN
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails elineUser = User.builder()
-                .username("eline")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                fennLazarusUser,
-                kimUser,
-                elineUser
-        );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+
+
+
+
+//    this is now done in the ApplicationUserService
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails fennLazarusUser = User.builder()
+//                .username("fennlazarus")
+//                .password(passwordEncoder.encode("password"))
+////                .roles(CUSTOMER.name()) // ROLE_CUSTOMER
+//                .authorities(CUSTOMER.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails kimUser = User.builder()
+//                .username("kim")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .authorities(ADMIN.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails elineUser = User.builder()
+//                .username("eline")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(
+//                fennLazarusUser,
+//                kimUser,
+//                elineUser
+//        );
+//    }
+
 }
