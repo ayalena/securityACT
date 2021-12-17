2dc1220bb0c6b2f94c61eb88aa2f9dcd46c98dd1
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-17 12:02:33 +0100

    added jwt token verifier filter

---
+++ src/main/java/com/example/securityACT/jwt/JwtTokenVerifier.java
@@ -0,0 +1,81 @@
+package com.example.securityACT.jwt;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+
+    //executed once per request (coming from client)
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        //get token from header
+        String authorizationHeader = request.getHeader("Authorization");
+
+        //reject if no header/doesn't start with bearer
+        if (Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        //if it does:
+        try {
+            //parse the token
+            String secretKey = "secureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKeysecureStrongAndVeryLongKey";
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .parseClaimsJws(token);
+
+            //get body
+            Claims body = claimsJws.getBody();
+
+            //retrieve subject from body (user!)
+            String username = body.getSubject();
+
+            //get authorities from body (list of maps)
+            var authorities = ((List<Map<String, String>>) body.get("authorities"));
+
+            //map authorities from token (needed to extend collection of authorities)
+            Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                    .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                    .collect(Collectors.toSet());
+
+            //tell spring security that this user from now on can be authenticated
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    simpleGrantedAuthorities);
+
+            //authenticate client that sent the token
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            //catch exception
+        } catch (JwtException e) {
+            throw new IllegalStateException(String.format("Token %s cannot be trusted", token));
+        }
+
+    }
+}
